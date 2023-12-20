48cc947d86b66205757ddad7ebd5e10c3a2eaeaa
Allison <allisonabinger@gmail.com>
2023-12-19 20:39:20 -0600

    Final Implementation from Allison 's Device

--- CuffinSeason/ask_who.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _on_click_screen_gui_input(event):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			get_tree().change_scene_to_file('res://select_date.tscn')
--- CuffinSeason/quiz_5.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _on_answer_yes_pressed():
-	PlayerAttributes.set_music('Yes')
-	get_tree().change_scene_to_file("res://quiz_answers.tscn")
-
-func _on_answer_no_pressed():
-	PlayerAttributes.set_music('No')
-	get_tree().change_scene_to_file("res://quiz_answers.tscn")
